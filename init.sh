539d74f7306caa3d9edfa8e449d79add072c26b9
Astrid <jean.jen@ya.ru>
2018-08-25 23:28:06 +0700

    fixed nginx.conf syntax errors and installed redirection

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
   sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
   sudo rm -rf /etc/nginx/sites-enabled/default
   sudo /etc/init.d/nginx restart
-  sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+  sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
   sudo gunicorn --config /home/box/web/etc/gunicorn.conf hello:app
