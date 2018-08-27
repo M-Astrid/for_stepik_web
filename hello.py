ecff51329c858b1dbd1eb758ae7b4b885a5a1601
Astrid <jean.jen@ya.ru>
2018-08-27 21:52:13 +0700

    developed wsgi for query string and fixed init.sh

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
+from cgi import parse_qs
+
 def app(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["Hello!"]
+	status = '200 OK'
+	headers = [('Content-Type', 'text/plain')]
+			   
+    qs = parse_qsl(env['QUERY_STRING'], encoding='utf-8') 
+			   
+	body = ''
+    for key, value in qs:
+        body += key + '=' + value + '\r\n'
+			   
+	start_response(status, headers)  
+    return body
