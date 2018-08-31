b0bcf5b769ac8b8e37b7c53f84a8d0cf2dad0206
Astrid <jean.jen@ya.ru>
2018-08-31 22:30:37 +0700

    fixed manager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,27 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class QuestionManager(models.Model):
-    def new(self):
-        return self.order_by('-added_at')
-    def popular(self):
-        return self.order_by('-rating')
-
 class Question(models.Model):
-    objects = QuestionManager()
 
-    title = models.CharField(max_length=255)
+	title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateField(blank=True, auto_now_add=True)
     rating = models.IntegerField(default = 0)
     author = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
     likes = models.ManyToManyField(User, related_name='likes_set')
 
-    def __unicode__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return '/question/%d/' % self.pk
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
 
 class Answer(models.Model):
 
