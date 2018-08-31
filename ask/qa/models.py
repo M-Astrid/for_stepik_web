b8d52f5226ad75047550e3a3c5766c757884dcb9
Astrid <jean.jen@ya.ru>
2018-08-31 16:34:19 +0700

    fixed models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -12,9 +12,9 @@ class Question(models.Model):
 
     title = models.CharField(max_length=255)
     text = models.TextField
-    added_at = models.DateField(blank = True, auto_now_add=True)
+    added_at = models.DateField(blank=True, auto_now_add=True)
     rating = models.IntegerField(default = 0)
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
     likes = models.ManyToManyField(User, related_name='likes_set')
 
     def __unicode__(self):
@@ -26,6 +26,6 @@ class Question(models.Model):
 class Answer(models.Model):
 
     text = models.TextField
-    added_at = models.DateField(blank = True, auto_now_add=True)
-    question = ForeignKeyField('Question')
-    author = models.ForeignKeyField(User)
+    added_at = models.DateField(blank=True, auto_now_add=True)
+    question = models.ForeignKey(Question, null=True, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
